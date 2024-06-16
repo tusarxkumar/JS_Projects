ec4657c4e4de43303879660b23a9a1b97fd2c948
Tusar kumar Parida <tusarkumarparida9@gmail.com>
2024-06-16 21:11:45 +0530

    Countdown timer project added

---
+++ 5-CountDown/script.js
@@ -0,0 +1,37 @@
+const endDate = "20 June 2024 10:00 PM";
+document.getElementById('end-date').innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
+function clock() {
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now) / 1000;
+
+    if (diff <= 0) {
+        celebrate();
+        return;
+    }
+
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+
+function celebrate() {
+    const celebrationMessage = document.createElement('div');
+    celebrationMessage.className = 'celebrate';
+    celebrationMessage.innerHTML = "🎉 It's time! 🎉";
+    document.querySelector('.main').appendChild(celebrationMessage);
+    
+    setTimeout(() => {
+        celebrationMessage.remove();
+    }, 2000);
+}
+
+// initial call
+clock();
+
+setInterval(() => {
+    clock();
+}, 1000);
